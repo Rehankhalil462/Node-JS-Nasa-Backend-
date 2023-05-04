7a58316715fafaab3bbc460825392696208faba9
Rehan Khalil <rehan.khalil@devways.digital>
2023-05-04 16:29:19 +0400

    Build pipeline workflow

--- server/src/app.js
+++ server/src/app.js
@@ -7,10 +7,10 @@ const api = require("./routes/api");
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("combined"));
-app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/v1", api);
 
 app.get("/", (req, res) => {
